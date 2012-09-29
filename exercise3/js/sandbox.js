2bbd5806debef83512b1be9527d4f49d08c2b8e4
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-09-29 18:17:01 +0530

    jQuery ex3

---
+++ exercise3/js/sandbox.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	// (i) Add five new list items to the end of the unordered list #myList.
+	var myItems = [], $myList = $('#myList');
+	for (var i=0; i<5; i++)
+		myItems.push('<li> New Item ' +i+ '</li>');
+	$myList.append(myItems.join(''));
+
+
+
+	// (ii) Remove the odd list items
+	$('#myList li:nth-child(odd)').remove();
+
+
+
+
+	// (iii) Add another h2 and another paragraph to the last div.module
+	$('div.module:last').append('<h2> This is the new heading added to the last div </h2>','<p> New para New para New para New para </p>').css("backgroundColor","orange");
+
+
+
+
+	// (iv) Add another option to the select element; give the option the value "Wednesday"
+	console.log($('#specials select').append("<option value='Wednesday'>Wednesday</option>"));
+
+
+
+
+	// (v) Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
+	console.log($("img[alt='fruit']").clone().appendTo($("body").append("<div class='module' id='new_div_id'")).insertAfter("#specials").css("border", "3px solid blue"));
+});
