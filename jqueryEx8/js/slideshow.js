8aa86cee84fd608d69f008a7ae3b4d0123021220
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-10-04 16:57:01 +0530

    Modified ex8

--- jqueryEx8/js/slideshow.js
+++ jqueryEx8/js/slideshow.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 	$("#nav_id").css("color","red");
 	var no_of_images = $("#slideshow li img").length;
 	$("#nav_id").html("Total Number of Images are : "+ no_of_images);
-    $("#slideshow li").hide();
+    	$("#slideshow li").hide();
 
-    $(function(){
-        $("#slideshow").children().eq(1).show().fadeOut(3000);
-        var i = 1;
-        setInterval(function(){
-        $("#nav_id").text("Image "+ (i+1) +" of "+no_of_images);
-        $("#slideshow li ").eq(i++).fadeIn(1000).delay(1000).fadeOut(1000);
-        if(i == 3)
-            i = 0;
-    },3005);
-  });
+    	$(function(){
+            $("#slideshow").children().eq(1).show().fadeOut(3000);
+            var i = 1;
+            setInterval(function(){
+            $("#nav_id").text("Image "+ (i+1) +" of "+no_of_images);
+            $("#slideshow li ").eq(i++).fadeIn(1000).delay(1000).fadeOut(1000);
+            if(i == 3)
+             i = 0;
+        },3005);
+    });
 });
