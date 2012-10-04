e1dd58fc568518017e1d7864d7f6c074e25a9089
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-10-04 16:45:00 +0530

    updated ex8

--- exercise8/js/slideshow.js
+++ exercise8/js/slideshow.js
@@ -1,23 +1,19 @@
 $(document).ready(function(){ 
 	$("#slideshow").insertBefore($("body").children(":first"));
-	$("#slideshow").addClass("fadeStyle");
 	$("<span> Navigation Area</span>").attr("id","nav_id").insertBefore("#slideshow");
 	$("#nav_id").css("color","red");
 	var no_of_images = $("#slideshow li img").length;
 	$("#nav_id").html("Total Number of Images are : "+ no_of_images);
-	var i = 1;
-	$(function(){
-    	$("#slideshow").children(":gt(0)").hide();
-    	//$("#slideshow").children().eq(1).hide();
-    	//$("#slideshow").children().eq(2).hide();
-    	setInterval(function(){
-    		//console.log(i++);
-    		$("#nav_id").text("Image "+ (++i) +" of "+no_of_images);
-      		$("#slideshow").children().eq(0).fadeOut()
-        	 .next().fadeIn()
-        	 .end().appendTo("#slideshow"); 
-        	 if(i == no_of_images) 
-        	 	i = 0;
-        },3000);
-	});
+    $("#slideshow li").hide();
+
+    $(function(){
+        $("#slideshow").children().eq(1).show().fadeOut(3000);
+        var i = 1;
+        setInterval(function(){
+        $("#nav_id").text("Image "+ (i+1) +" of "+no_of_images);
+        $("#slideshow li ").eq(i++).fadeIn(1000).delay(1000).fadeOut(1000);
+        if(i == 3)
+            i = 0;
+    },3005);
+  });
 });
