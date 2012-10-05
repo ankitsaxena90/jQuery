b4f8da4514c99be8b4cb93294daaf962e1e74b3f
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-10-05 17:13:51 +0530

    jq ex10

---
+++ exercise10/js/specials.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	var day_title = $("<title />");
+	var $new_div = $("<div id='div_id'></div>").insertAfter($("#specials form")); 
+  var day_text = $("<p></p>");
+  var day_image = $("<img />");
+  $new_div.append(day_title).append(day_text).append(day_image);
+	$("#specials select").change(function(){
+		var selected_day = $(this).val();
+		console.log(selected_day);
+		$.ajax({
+			url:'data/specials.json',
+			type:'Get',
+      cache: true,
+			dataType:'json',
+			success : function(response){
+
+				data = response;
+				var day = $(data).attr(selected_day);
+    			$("#div_id title").text($(day).attr("title"));
+         		$("#div_id").css("color",day.color)
+          		$("#div_id p").text($(day).attr("text"));
+          		$("#div_id img").attr("src",$(day).attr("image"));
+			},
+			error : function(xhr, status) {
+        		alert('Sorry, there was a problem!');
+    		},
+    		complete : function(xhr, status) {
+        		console.log('The request is complete!');
+    		}
+		})
+	});
+  $("#specials form").find("li.buttons").css("backgroundColor","red").remove();
+});
