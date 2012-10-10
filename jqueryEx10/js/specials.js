9c8565e60ace9c9731221e4a2707a8bef80dada3
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-10-10 15:02:48 +0530

    modified ex10

--- jqueryEx10/js/specials.js
+++ jqueryEx10/js/specials.js
@@ -1,12 +1,15 @@
+var selected_day;
 $(document).ready(function(){
-	var day_title = $("<title />");
+	var header = $("<h2></h2>");
 	var $new_div = $("<div id='div_id'></div>").insertAfter($("#specials form")); 
   	var day_text = $("<p></p>");
   	var day_image = $("<img />");
-  	$new_div.append(day_title).append(day_text).append(day_image);
+  	var run_ajax_once = true;
+  	$new_div.append(header).append(day_text).append(day_image);
 	$("#specials select").change(function(){
-		var selected_day = $(this).val();
+		selected_day = $(this).val();
 		console.log(selected_day);
+		if(run_ajax_once == true){
 		$.ajax({
 			url:'data/specials.json',
 			type:'Get',
@@ -14,11 +17,9 @@ $(document).ready(function(){
 			dataType:'json',
 			success : function(response){
 				data = response;
-				var day = $(data).attr(selected_day);
-				$("#div_id title").text($(day).attr("title"));
-				$("#div_id").css("color",day.color);
-				$("#div_id p").text($(day).attr("text"));
-				$("#div_id img").attr("src",$(day).attr("image"));
+				run_ajax_once = false;
+				display_data(data);
+				
 			},
 			error : function(xhr, status) {
         		alert('Sorry, there was a problem!');
@@ -26,7 +27,17 @@ $(document).ready(function(){
     		complete : function(xhr, status) {
         		console.log('The request is complete!');
     		}
-    	});
+		});
+		}else{
+			display_data(data);
+		}
 	});
   $("#specials form").find("li.buttons").css("backgroundColor","red").remove();
 });
+function display_data(obj){
+	var day = $(obj).attr(selected_day);
+    $("#div_id h2").text($(day).attr("title"));
+    $("#div_id").css("color",day.color);
+    $("#div_id p").text($(day).attr("text"));
+    $("#div_id img").attr("src",$(day).attr("image"));
+}
